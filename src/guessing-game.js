a4824af30b6608f8625c8f05b443165209713369
SLTS87 <maxim_107615@tut.by>
2018-01-12 16:45:26 +0300

    guessing-game/feature - implementation of required task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+	
+    constructor(){}
+		
+    setRange(min, max) 
+	{
+		this.min = min;
+		this.max = max;
     }
 
-    guess() {
-
+    guess()
+	{
+		if((this.max-this.min) % 2 == 0)
+		{
+			this.mid = this.min + ((this.max-this.min)/2);
+		}
+		
+		else
+		{
+			this.mid =  this.min + ((this.max-this.min)/2 + 0.5);
+		}
+		return this.mid;
     }
 
-    lower() {
-
+    lower() 
+	{
+		this.max = this.mid;
     }
 
-    greater() {
-
+    greater() 
+	{
+		this.min = this.mid;
     }
 }
 
